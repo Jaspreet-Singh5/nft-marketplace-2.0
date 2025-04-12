35653ebbf5c3a73e7e37a85123459b9dfc64855b
Jaspreet-Singh5 <jaspreetsingh@JaspreetSinghs-MacBook-Pro.local>
2025-04-12 14:23:51 +0530

    feat: init server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
