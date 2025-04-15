fa08559c572d640f661d383ef55bd4e950fc2170
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-04-15 20:24:22 +0530

    refactor: remove NFT model, contract service, and MongoDB connection from the server

--- server/app.js
+++ server/app.js
@@ -23,14 +23,6 @@ app.use(
     })
 );
 
-// Connect to MongoDB
-mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => {
-        console.error('MongoDB connection error:', err);
-    });
-
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/nft', nftRoutes);
