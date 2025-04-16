bfcfbf302f91933525fa2890ef324cc46b957d5e
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-04-16 23:56:00 +0530

    feat: add route to upload image to ipfs

--- server/routes/nft.js
+++ server/routes/nft.js
@@ -4,8 +4,19 @@ const { ethers } = require('ethers');
 const path = require('path');
 const fs = require('fs');
 const auth = require('../middleware/auth');
+const upload = multer();
+const { PinataSDK } = require('pinata');
+require("dotenv").config();
+
+const pinata = new PinataSDK({
+    pinataJwt: process.env.PINATA_JWT,
+    pinataGatewayKey: process.env.PINATA_GATEWAY_URL,
+});
 
 const router = express.Router();
 
+router.post('/upload', upload.single('image'), async (req, res) => {
+    const upload = await pinata.upload.public.file(new File([req.file.buffer], req.file.originalname));
+});
 
 module.exports = router;
