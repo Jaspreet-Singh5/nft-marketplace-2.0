0bfb393ec6789d0d9fc6efc68337e1d98f97bb8a
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-04-18 00:16:49 +0530

    feat: update Hardhat configuration to use environment variables for API keys and add network settings

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,13 +5,20 @@ const ETHERSCAN_API_KEY = vars.get('ETHERSCAN_API_KEY');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
-  etherscan: {
-    apiKey: {
-      sepolia: ETHERSCAN_API_KEY,
+    solidity: '0.8.28',
+    sourcify: {
+        enabled: true,
     },
-  },
-  sourcify: {
-    enabled: true,
-  },
+    networks: {
+        localhost: {},
+        sepolia: {
+            url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            accounts: privateKeys.split(','),
+        },
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
+    },
 };
