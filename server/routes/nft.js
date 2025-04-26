efaa24704c5a03bc23a9bbe1bc854fef42062a6d
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-04-26 17:17:01 +0530

    feat: enhance NFT upload route to include user data and transaction handling

--- server/routes/nft.js
+++ server/routes/nft.js
@@ -17,12 +17,14 @@ const pinata = new PinataSDK({
 const router = express.Router();
 
 const provider = new ethers.JsonRpcProvider(process.env.ETHEREUM_RPC_URL);
-const NFT = new ethers.Contract(process.env.NFT_CONTRACT_ADDRESS, NFT_ABI, provider);
+const nft = new ethers.Contract(process.env.NFT_CONTRACT_ADDRESS, NFT_ABI, provider);
+// signer for transactions
+const signer = new ethers.Wallet(process.env.MINTER_PRIVATE_KEY, provider);
 
 router.post('/upload', upload.single('image'), async (req, res) => {
     const { buffer } = req.file;
 
-    const { name, description } = req.body;
+    const { name, description, user } = req.body;
 
     // upload image to ipfs
     const { cid: imageCID } = await pinata.upload.public.file(new File([buffer], name)).keyvalues({
@@ -40,6 +42,12 @@ router.post('/upload', upload.single('image'), async (req, res) => {
     const { cid: metadataCID } = await pinata.upload.public.json(metadata);
 
     const tokenURI = `https://${process.env.PINATA_GATEWAY_URL}/ipfs/${metadataCID}`;
+    const transaction = await nft.connect(signer).createNft(user, tokenURI);
+    const result = await transaction.wait();
+    
+    res.status(200).json({
+        hash: result?.hash
+    });
 });
 
 module.exports = router;
