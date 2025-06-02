64a05c21955ebb50c8173ee146024024da2299b8
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-06-02 20:09:42 +0530

    feat: implement LoginWithEmail component for email-based authentication

---
+++ nftMarketplaceFrontend/src/LoginWithEmail.jsx
@@ -0,0 +1,20 @@
+import { Button, TextField } from '@mui/material';
+import { useLoginWithEmail } from '@privy-io/react-auth';
+import { useState } from 'react';
+import SendIcon from '@mui/icons-material/Send';
+
+export const LoginWithEmail = () => {
+    const { sendCode, loginWithCode } = useLoginWithEmail();
+    const [email, setEmail] = useState('');
+    const [code, setCode] = useState();
+
+    return (
+        <div className="h-100 m-2 flex justify-center items-center flex-col gap-2">
+            <TextField id="outlined-basic" label="Email" variant="outlined" onChange={e => setEmail(e.target.value)} className="w-100" />
+
+            <Button variant="contained" endIcon={<SendIcon />} onClick={e => sendCode({ email })} className="w-100">
+                Send Code
+            </Button>
+        </div>
+    );
+};
