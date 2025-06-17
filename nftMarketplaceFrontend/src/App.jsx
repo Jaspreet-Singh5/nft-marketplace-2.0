7e28eb78de46a960d57cecac710940b3419d3bd9
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-06-17 18:37:53 +0530

    feat: enhance App component to handle loading state, user authentication, and display user information

--- nftMarketplaceFrontend/src/App.jsx
+++ nftMarketplaceFrontend/src/App.jsx
@@ -1,19 +1,26 @@
 import './App.css';
 import { usePrivy } from '@privy-io/react-auth';
 import { LoginWithEmail } from './LoginWithEmail';
+import { CircularProgress } from '@mui/material';
 
 function App() {
-    const { ready } = usePrivy();
+    const { ready, authenticated, user } = usePrivy();
 
     if (!ready) {
-        return <div>loading...</div>;
-    }
-
-    return (
-        <>
-            <LoginWithEmail />
-        </>
-    );
+		return (
+            <div className='flex justify-center items-center h-100'>
+                <CircularProgress />
+            </div>
+        );
+    } else if (!authenticated) {
+		return (
+			<>
+				<LoginWithEmail />
+			</>
+		);
+	} else {
+		return <p>User {user?.id} is logged in.</p>;
+	}
 }
 
 export default App;
