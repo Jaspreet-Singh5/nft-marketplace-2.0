1cc2f5ba6343bd075bc69cb7ded38bf11db6ef0e
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-06-17 18:12:48 +0530

    fix: update event handling in LoginWithEmail component to use currentTarget for input values

--- nftMarketplaceFrontend/src/LoginWithEmail.jsx
+++ nftMarketplaceFrontend/src/LoginWithEmail.jsx
@@ -10,13 +10,13 @@ export const LoginWithEmail = () => {
 
     return (
         <div className="h-100 m-2 flex justify-center items-center flex-col gap-2">
-            <TextField id="outlined-basic" label="Email" variant="outlined" onChange={e => setEmail(e.target.value)} className="w-100" />
+            <TextField id="outlined-basic" label="Email" variant="outlined" onChange={e => setEmail(e.currentTarget.value)} className="w-100" />
 
             <Button variant="contained" endIcon={<SendIcon />} onClick={e => sendCode({ email })} className="w-100">
                 Send OTP
             </Button>
 
-            <TextField id="outlined-basic" label="Enter OTP" variant="outlined" onChange={e => setCode(e.target.value)} className="w-100" />
+            <TextField id="outlined-basic" label="Enter OTP" variant="outlined" onChange={e => setCode(e.currentTarget.value)} className="w-100" />
 
             <Button variant="contained" onClick={e => loginWithCode({ code })} className="w-100">
                 Login
