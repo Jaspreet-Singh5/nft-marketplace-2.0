fa694f14d0ea8bc7cc87eaf47ff9958d239cb95b
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-06-19 20:21:13 +0530

    feat: add file upload button to NFTForm component for enhanced NFT creation experience

--- nftMarketplaceFrontend/src/NFTForm.jsx
+++ nftMarketplaceFrontend/src/NFTForm.jsx
@@ -1,5 +1,18 @@
-import { Button, TextField } from '@mui/material';
+import { Button, styled, TextField } from '@mui/material';
 import { useState } from 'react';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
 
 export default function NFTForm() {
     const [walletAddress, setWalletAddress] = useState('');
@@ -25,6 +38,11 @@ export default function NFTForm() {
                 onChange={e => setDescription(e.currentTarget.value)}
                 className="w-100"
             />
+
+            <Button component="label" role={undefined} variant="contained" tabIndex={-1} startIcon={<CloudUploadIcon />} className="w-100">
+                Upload files
+                <VisuallyHiddenInput type="file" onChange={event => console.log(event.target.files)} accept="image/*" />
+            </Button>
 
             <Button variant="contained" className="w-100">
                 Create NFT
