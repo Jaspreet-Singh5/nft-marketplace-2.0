3314143a628d10f7a703047c3f9626b5a6637880
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-06-19 21:47:43 +0530

    feat: implement NFT creation form submission with axios for backend integration

--- nftMarketplaceFrontend/src/NFTForm.jsx
+++ nftMarketplaceFrontend/src/NFTForm.jsx
@@ -1,6 +1,7 @@
 import { Button, styled, TextField } from '@mui/material';
 import { useState } from 'react';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import axios from 'axios';
 
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -20,34 +21,56 @@ export default function NFTForm() {
     const [description, setDescription] = useState();
     const [image, setImage] = useState();
 
+    const createNft = event => {
+        event.preventDefault();
+
+        axios
+            .post(
+                `${import.meta.env.VITE_BACKEND_URL}/api/nft/upload`,
+                {
+                    image,
+                    walletAddress,
+                    name,
+                    description,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                }
+            )
+    };
+
     return (
-        <div className="h-100 m-2 flex justify-center items-center flex-col gap-2">
-            <TextField
-                id="outlined-basic"
-                label="Wallet address"
-                variant="outlined"
-                onChange={e => setWalletAddress(e.currentTarget.value)}
-                className="w-100"
-            />
+        <form enctype="multipart/form-data" onSubmit={e => createNft(e)}>
+            <div className="h-100 m-2 flex justify-center items-center flex-col gap-2">
+                <TextField
+                    id="outlined-basic"
+                    label="Wallet address"
+                    variant="outlined"
+                    onChange={e => setWalletAddress(e.currentTarget.value)}
+                    className="w-100"
+                />
 
-            <TextField id="outlined-basic" label="Name" variant="outlined" onChange={e => setName(e.currentTarget.value)} className="w-100" />
+                <TextField id="outlined-basic" label="Name" variant="outlined" onChange={e => setName(e.currentTarget.value)} className="w-100" />
 
-            <TextField
-                id="outlined-basic"
-                label="Description"
-                variant="outlined"
-                onChange={e => setDescription(e.currentTarget.value)}
-                className="w-100"
-            />
+                <TextField
+                    id="outlined-basic"
+                    label="Description"
+                    variant="outlined"
+                    onChange={e => setDescription(e.currentTarget.value)}
+                    className="w-100"
+                />
 
-            <Button component="label" role={undefined} variant="contained" tabIndex={-1} startIcon={<CloudUploadIcon />} className="w-100">
-                Upload files
-                <VisuallyHiddenInput type="file" onChange={event => setImage(event.target.files)} accept="image/*" />
-            </Button>
+                <Button component="label" role={undefined} variant="contained" tabIndex={-1} startIcon={<CloudUploadIcon />} className="w-100">
+                    Upload files
+                    <VisuallyHiddenInput type="file" onChange={event => setImage(event.target.files[0])} accept="image/*" />
+                </Button>
 
-            <Button variant="contained" className="w-100">
-                Create NFT
-            </Button>
-        </div>
+                <Button variant="contained" className="w-100" type="submit">
+                    Create NFT
+                </Button>
+            </div>
+        </form>
     );
 }
